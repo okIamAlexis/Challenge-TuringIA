1308584e5ffc63f38d81f1a0474043feeff15ed0
Alexis Molinero <alexis_molinero123@hotmail.com>
2022-07-01 18:55:07 -0500

    Ultimo Commit

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+// Menu
+let ubicacionPrincipal = window.pageYOffset;
+let nav = document.getElementsByTagName("nav");
+
+  AOS.init();
+
+const mov = window.addEventListener("scroll", function(){
+    let desplazaminetoActual = window.pageYOffset;
+
+    console.log(desplazaminetoActual);
+
+    if(ubicacionPrincipal >= desplazaminetoActual){
+        nav[0].style.top = "0px";
+
+    }else{
+        nav[0].style.top = "-200px"
+    }
+
+    ubicacionPrincipal =desplazaminetoActual;
+})
+
+
+const color = window.addEventListener("scroll", function(){
+  let desplazaminetoActual = window.pageYOffset;
+  let pix = 300;
+
+  console.log(desplazaminetoActual);
+
+  if(desplazaminetoActual >= pix){
+      colors();
+  }else{
+    nav[0].style.background = 'transparent';
+  }
+
+  ubicacionPrincipal =desplazaminetoActual;
+
+})
+
+function colors(){
+  let nav = document.getElementsByTagName("nav");
+  nav[0].style.background = 'black';
+  // const icon = document.getElementById('icon-search');
+  // icon.style.color = 'blue';
+  // const btn =  document.getElementsByid('talts');
+  // btn.style.background = 'white';
+}
 
 // Swiper
 var swiper = new Swiper(".swiper", {
